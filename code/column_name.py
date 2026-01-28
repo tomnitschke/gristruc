ccf60384fda3370e6bc19ab0c74a3c362562b177
tomnitschke <158305167+tomnitschke@users.noreply.github.com>
2026-01-28 16:42:41 +0100

    Update column_name.py

--- code/column_name.py
+++ code/column_name.py
@@ -1,18 +1,24 @@
-# Paste below into a formula column called 'column_name'.
+# Paste below into a formula column called 'tracked_name'.
 
-def column_name(fstring_expression_equal:str, item=1)->str:
+def tracked_name(self_documenting_fstring_expression):
   """
-  Get a referenced column's name (ID) while maintaining Grist dependencies (i.e. survives a column rename & ID change).
-
-  Usage:
-    LIB.lookupOne().column_name(f"{ $AnyColumn = }")
-
-  Insights:
-  f-string must evaluate the expression in the calling block, because the expression changes on block change.
-  Spaces inside the f-string are optional.
-  `$AnyColumn` and `rec.AnyColumn` are identical (Grist always translates former to latter).
-  Item `1` corresponds to pattern `rec.AnyColumn = ...`. Use items `2`+ to access referenced record's columns.
+  Get the name of a column, table, or both, such that it stays up to date when said column or table gets renamed by the user.
+  Args:
+    self_documenting_fstring_expression:  An f-string expression where you give the names of a column or table as they currently are.
+                                          This makes use of f-strings' self-documenting syntax and should look like this (note the equal sign):
+                                          f"{SomeTable=}"                          returns "SomeTable", but if said table gets renamed, will return the new name instead.
+                                          f"{$SomeColumn=}"                        returns "SomeColumn", but if said column gets renamed, will return the new name instead.
+                                          f"{SomeTable.lookupOne().SomeColumn=}"   returns ["SomeTable", "SomeColumn"], or their respective new names if the table or the column get renamed.
   """
-  return fstring_expression_equal.replace(".", " ").replace("=", " ").split()[item]
+  if '=' in self_documenting_fstring_expression:
+    left, right = self_documenting_fstring_expression.split('=')
+    left_parts = left.split('.')
+    if len(left_parts) == 1:
+      return left_parts[0]
+    if len(left_parts) == 2:
+      return left_parts[1]
+    if len(left_parts) == 3 and left_parts[1].startswith('lookupOne'):
+      return left_parts[0], left_parts[2]
+  raise ValueError(f"Passed f-string expression doesn't match any of the supported formats (or isn't the result of an f-string expression to begin with): '{self_documenting_fstring_expression}'. See docs for which formats are supported.")
 
-return column_name
+return tracked_name
